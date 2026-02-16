77ca73a1b3f18aa20154e1bbf0599c54b76b7880
DarwinMelaya <melayadarwin@gmail.com>
2026-02-17 01:23:21 +0800

    Frontend: Add login and signip page files

--- frontend/src/Routers/Routers.jsx
+++ frontend/src/Routers/Routers.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, UserLandingPage } from "../pages";
+import { Home, UserLandingPage, Login, SignUp } from "../pages";
 
 export const Routers = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* Auth */}
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signup" element={<SignUp />} />
         {/* User */}
         <Route path="/user/landing-page" element={<UserLandingPage />} />
       </Routes>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,8 @@
 export { default as Home } from "./home/Home";
 
+// auth
+export { default as Login } from "./auth/Login";
+export { default as SignUp } from "./auth/SignUp";
+
 // User
 export { default as UserLandingPage } from "./user/UserLandingPage";
